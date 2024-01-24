002bfcf7518cd515428f1bc0968f537af17aed2f
sathorn <sathorn@sheridancollege.ca>
2024-01-24 12:13:00 -0500

    Part 1 completed cat is moved with keyboardinput and using rigidbody2d add force cat blockers added to prevent cat from falling off the table

---
+++ Assets/Assignment/Scripts/Blackcat.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blackcat : MonoBehaviour
+{
+    Vector2 direction;
+    public Rigidbody2D rigidbody;
+    public float force = 10f;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+       
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.AddForce(direction * force * Time.deltaTime);
+    }
+
+}
