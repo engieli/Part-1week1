389a0164e6710033d01185442b26ffdbc4e12faf
Eng Sathorn <sathorn@shernet.sheridancollege.ca>
2024-01-25 18:28:33 -0500

    The glass now dissapears and reappears after falling and triggering fluffy cat

---
+++ Assets/Assignment/Scripts/Glassfall.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glassfall : MonoBehaviour
+{
+    public GameObject GlassPrefab;
+    public Transform spawn;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Ragdoll hit by" + collision.gameObject);
+
+        //spawn thing
+
+       
+        Instantiate(GlassPrefab, spawn.position, spawn.rotation);
+        
+        Destroy(gameObject);
+    }
+}
