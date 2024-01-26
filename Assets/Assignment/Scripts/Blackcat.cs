3e2173a5b59ff55ce008486f03d1658810fac0f8
Eng Sathorn <sathorn@shernet.sheridancollege.ca>
2024-01-25 20:09:24 -0500

    - Blackcat is now moving backwards after reaching cat blocker -OnCollisionEnter2D is used -OnTriggerEnter2D is used - transform.Translate

--- Assets/Assignment/Scripts/Blackcat.cs
+++ Assets/Assignment/Scripts/Blackcat.cs
@@ -7,6 +7,9 @@ public class Blackcat : MonoBehaviour
     Vector2 direction;
     public Rigidbody2D rigidbody;
     public float force = 10f;
+    public float force1 = 10f;
+    private bool collidided = false;
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -17,12 +20,27 @@ public class Blackcat : MonoBehaviour
     void Update()
     {
         direction.x = Input.GetAxis("Horizontal");
-       
+        if (collidided == true)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * force1);
+        }
+
     }
     private void FixedUpdate()
     {
         rigidbody.AddForce(direction * force * Time.deltaTime);
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("obsticle"))
+        {
+            collidided = true;
+        }
+        if (collision.gameObject.CompareTag("obstacle"))
+        {
+            collidided = false;
+        }
 
+    }
 
 }
